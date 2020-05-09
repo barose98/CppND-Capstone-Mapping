68e5d9432b67a4fc3cc760d6be79e324acb458ba
roseba <barrett.rose@gmail.com>
2020-05-09 11:40:42 -0400

    Feat: started concurrency work. See below.  creeate big map now spawns a new thread to get the OSM data. This thread will store the char's retrieved in the newly added CapstoneMappingQueue class. Then we can specify that queue as the data source for the Xerces parser. Which will be in another thread.

---
+++ include/CapstoneMappingQueue.h
@@ -0,0 +1,35 @@
+/*
+ * CapstoneMappingQueue.h
+ *
+ *  Created on: May 8, 2020
+ *      Author: roseba
+ */
+
+#ifndef SRC_CAPSTONEMAPPINGQUEUE_H_
+#define SRC_CAPSTONEMAPPINGQUEUE_H_
+
+#include <iostream>
+#include <thread>
+#include <queue>
+#include <future>
+#include <mutex>
+#include <algorithm>
+
+template <class T>
+class CapstoneMappingQueue {
+public:
+//    CapstoneMappingQueue();
+    void write(T msg, int num);
+    T read();
+    int getTotalSize();
+private:
+    std::mutex _mutex;
+    std::condition_variable _cond;
+    std::deque<T> _messages;
+    int total_size;
+};
+
+
+
+
+#endif /* SRC_CAPSTONEMAPPINGQUEUE_H_ */
---
+++ src/CapstoneMappingQueue.cpp
@@ -0,0 +1,23 @@
+/*
+ * CapstoneMappingQueue.cpp
+ *
+ *  Created on: May 8, 2020
+ *      Author: roseba
+ */
+
+#include "CapstoneMappingQueue.h"
+
+template<class T>
+inline void CapstoneMappingQueue<T>::write(T msg, int num)
+{
+}
+
+template<class T>
+inline T CapstoneMappingQueue<T>::read()
+{
+}
+
+template<class T>
+inline int CapstoneMappingQueue<T>::getTotalSize()
+{
+}
